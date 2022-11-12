22b3582d933bae3aa25f9c2763369fe1757f3179
kavya <kavya@sili54>
2022-11-12 22:50:07 +0100

    pushing code first time

---
+++ Project_1/src/com/Package1/Assignment/Day1Oct29Asmt.java
@@ -0,0 +1,27 @@
+package com.Package1.Assignment;
+
+public class Day1Oct29Asmt {
+	int rollNo;
+	int age;
+	
+	public void display1()
+	{
+		System.out.println("Welcome to all of you"); 
+		
+	}
+	public void display2()
+	{
+		System.out.println("Automation is very easy");
+		
+	}
+
+	public static void main(String[] args) {
+		Day1Oct29Asmt s1 = new Day1Oct29Asmt();
+		s1.age= 17;
+		s1.rollNo = 12345;
+		s1.display1();
+		s1.display2();
+		
+		System.out.println("Age is : " + s1.age + "\nrollnumber is  : " + s1.rollNo);
+	}
+}
---
+++ Project_1/src/com/Package1/Assignment/Day2Oct30Asmt.java
@@ -0,0 +1,43 @@
+package com.Package1.Assignment;
+
+public class Day2Oct30Asmt {
+	
+		float a;
+		float b;
+		
+		public float sum (float a , float b) {
+			System.out.println("Sum of numbers is " + (a+b));
+			return a+b;
+		 	
+		}
+		public float sub (float a , float b){
+			System.out.println("Diff of numbers is " + (a-b));
+			return a-b;
+		 	
+		}
+		public float mul (float a , float b){
+			System.out.println("Multiplication of numbers is " + (a*b));
+			return (a*b);
+		 	
+		}
+		public float div (float a , float b){
+			System.out.println("division of numbers is " + (a/b));
+			return (a/b);
+		 	
+		}
+		
+		public static void main(String[] args)
+		{
+			Day2Oct30Asmt arth_op = new Day2Oct30Asmt();
+			
+			float result1 = arth_op.div(arth_op.mul(arth_op.sub(arth_op.sum(arth_op.sum(10, 2), 2), 2), 2), 2);
+			System.out.println("ASSIGNMENT1 --> Arthmetic operation result : " + result1 + "\n");
+			
+			float result2 = arth_op.div(arth_op.sub(arth_op.sub(arth_op.sum(arth_op.mul(10, 2),2),2),2),2);
+			System.out.println("ASSIGNMENT2 --> Arthmetic operation result : " + result2);
+			
+			
+		}
+	
+
+}
